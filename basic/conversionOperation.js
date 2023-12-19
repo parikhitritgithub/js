e9ac5ed4f48b01bd223912e11a2e4b113015b302
Parikhit kurmi <124444610+parikhitritgithub@users.noreply.github.com>
2023-12-19 05:23:17 +0000

    js

---
+++ basic/conversionOperation.js
@@ -0,0 +1,25 @@
+// let score = "33"
+// console.log(typeof score)
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+//"33" => 33
+//"33abc" => NaN (type number )
+// true => 1 / false => 0 ;
+
+let isLoggedIn = "parikhit" 
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn) ; 
+
+//1 => true 0 => false
+//"" => false 
+//"parikhit" => true 
+
+let someNumber = 33 
+let stringNumber = Number(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
+
---
+++ basic/datatypes.js
@@ -0,0 +1,27 @@
+"use strict"// treat all js code as newer version 
+
+//alert(3+3) // we are using node js not browser 
+//code readebility should be high 
+console.log(3+3) 
+console.log("parikhit")
+
+let name = "parikhit"
+let age = 18 
+let isLoggedIn  = false 
+let state = null 
+ 
+
+// primitive datatype ----> 
+// number => 2 to power 53 
+// bigint
+// string =>""
+// boolean => true / false 
+// null => standalone value 
+// undefine => 
+// symbole => use in react js  // unique 
+console.log(typeof age)
+console.log(typeof null)// null ---> object 
+console.log(typeof undefined ); // undefined 
+//TC39
+
+//object 
